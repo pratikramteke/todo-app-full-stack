10143db266976751b1446a4d146a03e9e1347530
Pratik Ramteke <pratikramteke222@gmail.com>
2023-04-16 14:56:08 +0530

    api tested

--- server/server.js
+++ server/server.js
@@ -8,10 +8,14 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/test")
   .then(() => console.log("Connected!"))
 
+app.get('/', (req, res) => {
+
+  res.status(200).send('hi')
+})
 
 app.post('/create', (req, res) => {
   console.log(req.body);
-  res.json({ data: req.body })
-  User.create({name:'pratik'})
+  res.json({ data: req.body, sucess:true })
+  // User.create({name:'pratik'})
 })
 app.listen(3000)
