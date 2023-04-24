f6bf0f191218ffa06657059f906a5ef8a9c6d09f
Pratik Ramteke <pratikramteke222@gmail.com>
2023-04-24 19:43:01 +0530

    remove mongo

--- server/server.js
+++ server/server.js
@@ -6,32 +6,21 @@ import mongoose from "mongoose"
 const app = express()
 app.use(express.json())
 app.use(cors())
-// (async () => {
-//   try {
-//     await mongoose.connect(config.MONGODB_URL)
-//     console.log("DB Connected")
-//     app.on("error", (err) => {
-//       console.log("ERROR:", err)
-//       throw err
-//     })
-//     app.listen(config.PORT, () =>
-//       console.log(`App listening on port ${config.PORT}`)
-//     )
-//   } catch (err) {
-//     console.error("ERROR: ", err)
-//   }
-// })()
-
-mongoose.connect("mongodb://localhost:27017/todo")
-
-mongoose.connection.once("open", function () {
-  console.log("MongoDB database connection established successfully")
-})
-
-const Cat = mongoose.model("Cat", { name: String })
-
-const kitty = new Cat({ name: "Zildjian" })
-kitty.save().then(() => console.log("meow"))
+(async () => {
+  try {
+    await mongoose.connect(config.MONGODB_URL)
+    console.log("DB Connected")
+    app.on("error", (err) => {
+      console.log("ERROR:", err)
+      throw err
+    })
+    app.listen(config.PORT, () =>
+      console.log(`App listening on port ${config.PORT}`)
+    )
+  } catch (err) {
+    console.error("ERROR: ", err)
+  }
+})()
 
 app.listen(config.PORT, () =>
   console.log(`App listening on port ${config.PORT}`)
